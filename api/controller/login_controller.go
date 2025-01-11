17f40a485409af3d829543e80cc9184645134750
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-01-11 17:54:12 +0530

    feat: login routers

--- api/controller/login_controller.go
+++ api/controller/login_controller.go
@@ -9,33 +9,33 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func (loginController *LoginController) Login(c echo.Context){
+func (loginController *LoginController) Login(c echo.Context)error{
 	var request domain.LoginRequest
 	err := c.Bind(&request)
 	ctx := utils.ExtractContext(c)
 	if err != nil {
-		c.JSON(http.StatusBadRequest,domain.ErrorResponse{Message: "Invalid request",StatusCode: http.StatusBadRequest})
+		return c.JSON(http.StatusBadRequest,domain.ErrorResponse{Message: "Invalid request",StatusCode: http.StatusBadRequest})
 	}
 	user, err := loginController.LoginUsecase.GetUserByUserName(ctx, request.UserName)
 	if err != nil {
-		c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "User not found with given credentials", StatusCode: http.StatusUnauthorized})
+		return c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "User not found with given credentials", StatusCode: http.StatusUnauthorized})
 	}
 	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password)) != nil {
-		c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "Invalid credentials", StatusCode: http.StatusUnauthorized})
+		return c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "Invalid credentials", StatusCode: http.StatusUnauthorized})
 	}
 	accessToken , err := loginController.LoginUsecase.CreateAccessToken(&user,loginController.Env.AccessTokenSecretKey,loginController.Env.AccessTokenExpiryHour)
 	if err != nil{
-		c.JSON(http.StatusInternalServerError,domain.ErrorResponse{Message: err.Error(), StatusCode: http.StatusInternalServerError})
+		return c.JSON(http.StatusInternalServerError,domain.ErrorResponse{Message: err.Error(), StatusCode: http.StatusInternalServerError})
 	}
 	refreshToken ,err := loginController.LoginUsecase.CreateRefreshToken(&user, loginController.Env.RefreshTokenSecretKey, loginController.Env.RefreshTokenExpiryHour)
 	if err != nil{
-		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error(), StatusCode: http.StatusInternalServerError})
+		return c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error(), StatusCode: http.StatusInternalServerError})
 	
 	}
 	loginResponse := domain.LoginResponse{
 		AccessToken: accessToken,
 		RefreshToken: refreshToken,
 	}
-	c.JSON(http.StatusOK, loginResponse)
+	return c.JSON(http.StatusOK, loginResponse)
 
 }
---
+++ api/route/login_router.go
@@ -0,0 +1,26 @@
+package route
+
+import (
+	"time"
+
+	"github.com/ShyamSundhar1411/chime-space-go-backend/api/controller"
+	"github.com/ShyamSundhar1411/chime-space-go-backend/bootstrap"
+	"github.com/ShyamSundhar1411/chime-space-go-backend/domain"
+	"github.com/ShyamSundhar1411/chime-space-go-backend/mongo"
+	"github.com/ShyamSundhar1411/chime-space-go-backend/repository"
+	"github.com/ShyamSundhar1411/chime-space-go-backend/usecase"
+	"github.com/labstack/echo/v4"
+)
+
+func NewLoginRouter(env *bootstrap.Env, timeout time.Duration,db mongo.Database,routerGroup *echo.Group){
+	userRepository := repository.NewUserRepository(db, domain.CollectionUser)
+	loginController := &controller.LoginController{
+		LoginUsecase: usecase.NewLoginUsecase(
+			userRepository,
+			timeout,
+		),
+		Env: env,
+	}
+	routerGroup.POST("/auth/login/",loginController.Login)
+
+}
