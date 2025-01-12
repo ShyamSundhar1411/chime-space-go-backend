4f0bad74f77ae5bdc54e4e1cdd45ae9165a86521
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-01-13 00:00:02 +0530

    feat: added signup routers

---
+++ api/route/signup_router.go
@@ -0,0 +1,23 @@
+package route
+
+import (
+	"time"
+
+	"github.com/ShyamSundhar1411/chime-space-go-backend/api/controller"
+	"github.com/ShyamSundhar1411/chime-space-go-backend/bootstrap"
+	"github.com/ShyamSundhar1411/chime-space-go-backend/domain"
+	"github.com/ShyamSundhar1411/chime-space-go-backend/mongo"
+	"github.com/ShyamSundhar1411/chime-space-go-backend/repository"
+	"github.com/ShyamSundhar1411/chime-space-go-backend/usecase"
+	"github.com/labstack/echo/v4"
+)
+
+func NewSignUpRouter(env *bootstrap.Env, timout time.Duration, db mongo.Database, routerGroup *echo.Group){
+	userRepository := repository.NewUserRepository(db, domain.CollectionUser)
+	signUpController := &controller.SignUpController{
+		SignUpUsecase: usecase.NewSignUpUsecase(userRepository, timout),
+		Env: env,
+	}
+	routerGroup.POST("/signup", signUpController.SignUp)
+	
+}
